e2a59d49f7de7e4261160960491f21deadd21149
CoilDomain <xorphenochx@gmail.com>
2020-08-26 14:34:15 -0400

    Restructed code, added the code to enable multiple tags to be inputted

--- src/main.go
+++ src/main.go
@@ -6,68 +6,57 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+// Create function to grab content of page and image URLs
+func datascrape(URL string) {
+	// Getting the content of the page
+	res, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	// Searching for the images URLs
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find(".post-preview").Each(func(i int, s *goquery.Selection) {
+		URL, _ := s.Attr("data-file-url")
+		fmt.Printf("%s\n", URL)
+	})
+}
+
+// Main function
 func main() {
 	// Commandline arguments
 	tagPtr := flag.String("tag", "robot", "Any Danbooru tag")
 	pagesPtr := flag.String("pages", "1", "Number of pages to go through")
 	flag.Parse()
+
 	// Creating the proper URL to scrape
 	baseURL := "https://danbooru.donmai.us/posts?"
-	pages := "page=" + *pagesPtr
-	tags := "&tags=" + *tagPtr
-	fullURL := baseURL + pages + tags
-	// Creating a variable to use as the page int
-	p, _ := strconv.Atoi(*pagesPtr)
-	// Check whether p is larger than 1 so I can do a sequence for pages before p
-	if p > 1 {
-		// Do a loop based on how many pages are between 1 and the actual argument
+
+	// Create variable to split the tags into separate strings to use in a foreach statement
+	tags := strings.Split(*tagPtr, ",")
+	// Create loop for multiple tags
+	for _, tag := range tags {
+		// Convert the page argument variable into an into so I can do a numeric compare
+		p, _ := strconv.Atoi(*pagesPtr)
+		// Run loop on pages to back track from the selected page
 		for n := 1; n < p+1; n++ {
 			// Creating the proper URL to scrape
 			pns := strconv.Itoa(n)
-			pages := "page=" + pns
-			fullURL := baseURL + pages + tags
-			// Getting the content of the page
-			res, err := http.Get(fullURL)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer res.Body.Close()
-			if res.StatusCode != 200 {
-				log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-			}
-			// Searching for the images
-			doc, err := goquery.NewDocumentFromReader(res.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			doc.Find(".post-preview").Each(func(i int, s *goquery.Selection) {
-				URL, _ := s.Attr("data-file-url")
-				fmt.Printf("%s\n", URL)
-			})
+			pageURL := "page=" + pns
+			tagURL := "&tags=" + tag
+			fullURL := baseURL + pageURL + tagURL
+			datascrape(fullURL)
 		}
-	} else {
-		// Getting the content of the page
-		res, err := http.Get(fullURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer res.Body.Close()
-		if res.StatusCode != 200 {
-			log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-		}
-		// Searching for the images
-		doc, err := goquery.NewDocumentFromReader(res.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		doc.Find(".post-preview").Each(func(i int, s *goquery.Selection) {
-			URL, _ := s.Attr("data-file-url")
-			fmt.Printf("%s\n", URL)
-		})
 	}
-
 }
